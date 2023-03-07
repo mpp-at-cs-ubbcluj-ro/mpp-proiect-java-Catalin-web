9138650ca4d8d86c64c1d8242b63e8f0f3c3cc11
Bugnar Catalin <catalin.bugnar@yahoo.ro>
2023-03-07 20:42:42 +0200

    first commit

---
+++ proiect/src/main/java/grup/Domain/Entity.java
@@ -0,0 +1,17 @@
+package grup.Domain;
+
+public class Entity {
+    public Entity(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+}
